11d452b08bd58d64b7f609a5859a4f0acebcd253
Elia Schito <perlelia@gmail.com>
2012-02-14 17:15:50 +0100

    Better output

--- lib/heroku-yml-config/heroku/command/yml_config.rb
+++ lib/heroku-yml-config/heroku/command/yml_config.rb
@@ -9,11 +9,10 @@ class Heroku::Command::Config
     
     config_file_path = args.shift
     config_file_path = File.expand_path(config_file_path)
+    raise CommandFailed, "Usage: heroku config:upload YML_FILENAME" unless File.exist? config_file_path
+    
     config_hash = YAML.load_file(config_file_path)
     vars = config_hash.inject({}) { |h, (k,v)| h[k.upcase]=v.to_s; h}
-    
-    display "Found config vars...", true
-    display_vars(vars, :long => true, :shell => true)
     
     # try to get the app to fail fast
     detected_app = app
