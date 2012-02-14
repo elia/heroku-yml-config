7391910b2f85733ad4b1542c9a031ee22f6c632e
Elia Schito <perlelia@gmail.com>
2012-02-14 16:38:37 +0100

    Vanilla bundler gem

---
+++ lib/heroku-yml-config.rb
@@ -0,0 +1,9 @@
+require "heroku-yml-config/version"
+
+module Heroku
+  module Yml
+    module Config
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/heroku-yml-config/version.rb
@@ -0,0 +1,7 @@
+module Heroku
+  module Yml
+    module Config
+      VERSION = "0.0.1"
+    end
+  end
+end
